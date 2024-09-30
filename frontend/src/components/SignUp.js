b162fdf4b3d1be512a388030d0ecb43b4cb67bdb
harjotbasota <harjotsinghbasota@gmail.com>
2024-09-30 12:48:49 -0400

    sign up page ui

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -6,7 +6,7 @@ import { MovieContext } from '../context/moviesContext';
 const SignUp = () => {
     const [password1, setPassword1] = useState('');
     const [password2,setPassword2] = useState('');
-    const [invalidCredentails,setInvalidCredentails] = useState(true);
+    const [invalidCredentails,setInvalidCredentails] = useState(false);
     const {userName, setUserName, userEmail, setUserEmail } = useContext(MovieContext);
     
     const handleSignUpFormSubmission = (e)=>{
@@ -14,6 +14,19 @@ const SignUp = () => {
         const formdata = new FormData(e.target);
         setUserName(formdata.get('Username'));
         setUserEmail(formdata.get('userEmail'));
+        if(password1!=password2){
+          setInvalidCredentails(true);
+        }else{
+          setInvalidCredentails(false);
+        }
+    }
+    const handlePasswordInput = (e)=>{
+      if(e.target.name == 'password1'){
+        setPassword1(e.target.value);
+      }
+      if(e.target.name == 'password2'){
+        setPassword2(e.target.value);
+      }
     }
 
   return (
@@ -21,16 +34,16 @@ const SignUp = () => {
       
       <form onSubmit={handleSignUpFormSubmission}>
         <h1> SignUp</h1>
-        <label>Username</label>
-        <input type='text' name='Username'></input>
-        <label>Email</label>
-        <input type='text' name='userEmail'></input>
-        <label>password</label>
-        <input type='password' name='password1' ></input>
-        <label>Confirm Password</label>
-        <input type='password' name='password2' ></input>
+        <label htmlFor='Username'>Username</label>
+        <input type='text' name='Username' placeholder='Enter username'></input>
+        <label htmlFor='userEmail'>Email</label>
+        <input type='text' name='userEmail' placeholder='xyz@myemail.com'></input>
+        <label htmlFor='password1'>password</label>
+        <input type='password' name='password1' placeholder='********' onChange={handlePasswordInput}></input>
+        <label htmlFor='password2'>Confirm Password</label>
+        <input type='password' name='password2' placeholder='********' onChange={handlePasswordInput} ></input>
         { invalidCredentails? <p style={{color: 'red'}}> Password must be same!!!!  </p> : null}
-        <input type='submit' value='Sign Up' disabled={invalidCredentails}></input>
+        <button type='submit' className='submitButton'>SignUp</button>
         <p> Already have an account? <Link to='/Login'>login</Link> </p>
       </form>
     </div>
