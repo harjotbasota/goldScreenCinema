89f63d681b22a887229e0d1539689adb4550dacc
harjotbasota <harjotsinghbasota@gmail.com>
2024-10-02 10:26:20 -0400

    movie detail page ui

--- frontend/src/components/MovieDetail.js
+++ frontend/src/components/MovieDetail.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import { MovieContext } from '../context/moviesContext';
+import '../styles/MovieDetail.css';
 
 const MovieDetail = () => {
     const location = useLocation();
@@ -8,27 +9,34 @@ const MovieDetail = () => {
     const cinemasdata = useContext(MovieContext);
     console.log(cinemasdata.cinemas);
   return (
-    <div className='movieDetail' style={{marginTop:'7.5vh',height:'85vh'}}>
-      <p className='infoHeader'> Title: <span className='info'>{movie.title}</span> </p>
-      <p className='infoHeader'> Rating: <span className='info'>{movie.rating}</span> </p>
-      <p className='infoHeader'> Description: <span className='info'>{movie.description}</span> </p>
-      <p className='infoHeader'> Genre: <span className='info'>{movie.genre}</span> </p>
-      <p className='infoHeader'> Release Date: <span className='info'>{movie.release_date}</span> </p>
-      <p className='infoHeader'> Ticket Price: <span className='info'>{movie.ticketPrice}</span> </p>
-      <p> </p>
-      {
-        Object.entries(movie.cinema_shows).map(([cinema,shows])=>{
-            return Object.entries(cinemasdata.cinemas).map(([cinemaid,cinemadetails])=>{
-                return cinemaid == cinema -1 ? <div> 
-                    <p style={{color: 'red'}}>{cinemadetails.name} {cinemadetails.location} </p> 
-                    <p> {shows.join(' , ')} </p>
-                    
-                    
-                    
-                    </div> : null
+    <div className='movieDetailPage'>
+      <div className='movieDetailContainer'>
+      <div className='containerLeftSide'>
+          <span className='title'>{movie.title}</span>
+          <p className='infoHeader'> Rating: <span className='info'>{movie.rating}</span>   </p>
+          <p className='infoHeader'> Description: <span className='info'>{movie.description}</span>  </p> 
+          <p className='infoHeader'> Genre:  <span className='info'>{movie.genre}</span>   </p> 
+          <p className='infoHeader'> Release Date:  <span className='info'>{movie.release_date}</span>  </p> 
+          <p className='infoHeader'> Ticket Price:  <span className='info'>${movie.ticketPrice}</span>  </p>
+          <p className='infoHeader2'> Availabe Shows in your nearby cinemas</p>
+          {
+            Object.entries(movie.cinema_shows).map(([cinema,shows])=>{
+                return Object.entries(cinemasdata.cinemas).map(([cinemaid,cinemadetails])=>{
+                    return cinemaid == cinema -1 ? <div> 
+                        <p className='cinemaName'>{cinemadetails.name} {cinemadetails.location} </p> 
+                        <p className='showsDetail'> {shows.join(' , ')} </p>                 
+                        
+                        </div> : null
+                })
             })
-        })
-      }
+          }
+          <button className='bookTicketButton'> BOOK TICKET </button>
+      </div>
+      <div className='containerRightSide'>
+          <img src={movie.poster} />
+      </div>
+      </div>
+
     </div>
   )
 }
