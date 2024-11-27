1568c82fdd9e97d8dce79d1019061c5d9700bf28
harjotbasota <harjotsinghbasota@gmail.com>
2024-11-27 17:32:24 -0500

    setting up git hub actions pipeline

--- generateSSL.sh
+++ generateSSL.sh
@@ -7,12 +7,12 @@ sudo certbot certonly --standalone -d "$DOMAIN_NAME"
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 mkdir $SCRIPT_DIR/api/certs
-cd "./api/certs"
+cd $SCRIPT_DIR/api/certs
 cp /etc/letsencrypt/live/gsc.harjotbasota.com/privkey.pem .
 cp /etc/letsencrypt/live/gsc.harjotbasota.com/fullchain.pem .
 
 mkdir $SCRIPT_DIR/frontend/nginx/certs
-cd "./frontend/nginx/certs"
+cd $SCRIPT_DIR/frontend/nginx/certs
 cp /etc/letsencrypt/live/gsc.harjotbasota.com/privkey.pem .
 cp /etc/letsencrypt/live/gsc.harjotbasota.com/fullchain.pem .
 
