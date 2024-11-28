a6a71540da0f768bb6aaf1558efe28a4612d0003
harjotbasota <harjotsinghbasota@gmail.com>
2024-11-27 21:45:43 -0500

    updated domain

--- generateSSL.sh
+++ generateSSL.sh
@@ -8,12 +8,12 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 mkdir $SCRIPT_DIR/api/certs
 cd $SCRIPT_DIR/api/certs
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/privkey.pem .
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/fullchain.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/privkey.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/fullchain.pem .
 
 mkdir $SCRIPT_DIR/frontend/nginx/certs
 cd $SCRIPT_DIR/frontend/nginx/certs
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/privkey.pem .
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/fullchain.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/privkey.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/fullchain.pem .
 
 echo "SSL certificated issued and successfully attached to nginx"
--- renewSSL.sh
+++ renewSSL.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 cd $SCRIPT_DIR/api/certs
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/privkey.pem .
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/fullchain.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/privkey.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/fullchain.pem .
 
 cd $SCRIPT_DIR/frontend/nginx/certs
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/privkey.pem .
-cp /etc/letsencrypt/live/gsc.harjotbasota.com/fullchain.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/privkey.pem .
+cp /etc/letsencrypt/live/goldscreencinema.harjotbasota.com/fullchain.pem .
 
 echo "SSL certificated files are updated"
